73269a3539d07290997b533afd22804e4355a9a5
Gonzalo Padilla Eguiluz <gpadilla@mainforcp.local>
2025-07-02 12:09:00 +0200

    Primer commit de EXPEDICION

--- pages/admisiones_main.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def academica_page():
-    st.markdown("<h1>Área Académica</h1>", unsafe_allow_html=True)
-    st.warning("La sección de Área Académica está en desarrollo.")
-    st.info("Por ahora, solo está implementada la sección de Deuda.")
